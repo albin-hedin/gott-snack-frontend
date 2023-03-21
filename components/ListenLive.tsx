dc2f0e3ba7705691ed3063d89726e89a9294c4e6
Albin Hedin <albin.hedin@ingka.com>
2023-03-21 16:27:03 +0100

    reorder listen now section

--- components/ListenLive.tsx
+++ components/ListenLive.tsx
@@ -4,50 +4,54 @@ import SpotifyPlayer from '@/components/SpotifyPlayer'
 
 const ListenLive = () => {
   return (
-    <div className='flex flex-col items-center mt-2'>
+    <div>
       <div className='mb-5 mx-3'>
-        <div className='grid md:grid-cols-2 grid-cols-1 md:gap-24 gap-4'>
-          <div>
-            <div className='mb-4'>
-              <MixlrPlayer />
-            </div>
-            {/* <div className='grid grid-cols-2'>
-              <div>
+        <div className='
+         grid
+         grid-cols-1 
+         gap-4
+         md:grid-cols-3 
+         md:gap-6
+         md:mx-6'>
+          <div className='flex flex-col items-center'>
+            <MixlrPlayer />
+            <div className='flex flex-row mt-2'>
+              <div className='mr-1'>
                 <a href="https://play.google.com/store/apps/details?id=com.mixlr.android&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1" target='_blank'>
                   <Image
                     src='/androidstore.png'
                     alt=''
-                    width={200}
-                    height={150} />
+                    width={150}
+                    height={130} />
                 </a>
               </div>
-              <div className='mb-5'>
+              <div>
                 <a href="https://apps.apple.com/se/app/mixlr-social-live-audio/id583705714?fbclid=IwAR3afb1zKBIfucv7C_1R-ML_hhlnMP6YJzyh6fK7JksL9WYXnzhuZH2wiEg" target='_blank'>
                   <Image
                     src='/applestore.png'
                     alt=''
-                    width={200}
-                    height={150} />
+                    width={150}
+                    height={130} />
                 </a>
               </div>
-            </div> */}
-            <div>
-              <SpotifyPlayer />
             </div>
+          </div>
+          <div >
+            <SpotifyPlayer />
           </div>
           <div className=''>
             <Image
               src='/senaste.jpg'
               alt=''
-              width={665}
-              height={1300} />
-          </div>
-          <div>
+              width={465}
+              height={1000} />
           </div>
         </div>
       </div>
     </div>
   )
 }
+
+
 
 export default ListenLive 
