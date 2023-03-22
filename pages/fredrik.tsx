a7ab71db1493a07bfe8ae2e2e75d1d7037fdb8d3
Albin Hedin <albin.hedin@ingka.com>
2023-03-22 21:23:46 +0100

    fix padding on about pages

--- pages/fredrik.tsx
+++ pages/fredrik.tsx
@@ -6,8 +6,15 @@ const JesperAbout = () => {
     <div id='top' className='flex flex-col items-center'>
       <div className='text-center mb-10'>
         <LargeHeader headerText='Fredrik "Geniet" Söderholm' />
-        <div className='px-44'>
-          Fredrik Geniet Söderholm grundade Gott Snack med ambitionen att göra morgonradio på sitt sätt. Tidigare känd från Sveriges Radio och har efter Gott Snacks framfart också gjort poddar som Sorry - Allt gick åt helvete med Ola Rapace, Schulman och Geniet med Calle Schulman och den nya klockdotterpodden till Gott Snack- Ur-Gott Snack. Geniet programleder livesändningarna i Gott Snack.
+        <div className='md:px-44 px-4'>
+          Fredrik Geniet Söderholm grundade Gott Snack med ambitionen
+          att göra morgonradio på sitt sätt.
+          <br />
+          <br />
+          Tidigare känd från Sveriges Radio och har efter Gott Snacks framfart
+          också gjort poddar som Sorry - Allt gick åt helvete med Ola Rapace, Schulman och Geniet med Calle Schulman och den nya
+          klockdotterpodden till Gott Snack- Ur-Gott Snack. Geniet programleder
+          livesändningarna i Gott Snack.
         </div>
       </div>
       <ImageWithTitle
