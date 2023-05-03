0aeab0c0bee47f67ac347ef83e08bb495c1b4f82
Albin Hedin <albin.hedin@ingka.com>
2023-05-03 20:25:43 +0200

    fixes bg pic on festival banner

--- components/FestivalBanner.tsx
+++ components/FestivalBanner.tsx
@@ -1,26 +1,41 @@
 import Link from 'next/link'
 import SectionHeader from './SectionHeader'
 import Image from 'next/image'
+import { isMobile } from 'react-device-detect'
 
 const FestivalBanner = () => {
+  const renderBackgroundImageClass = () => {
+    if (isMobile) {
+      return {
+        backgroundImage: 'url(/gs-festival-no-text.jpg)',
+        backgroundPosition: '95% 44%',
+        backgroundSize: '370px'
+      }
+    } else {
+      return {
+        backgroundImage: 'url(/gs-festival-no-text.jpg)',
+        backgroundPosition: '100% 33.5%',
+        backgroundSize: '1300px'
+      }
+    }
+  }
+
   return (
     <>
-      <div className='text-center flex flex-col items-center' style={{
-        backgroundImage: 'url(/gs-festival-no-text.jpg)',
-        backgroundPosition: '85% 44%',
-      }}>
+      <div className={`text-center flex flex-col items-center md:festival-bg-image festival-bg-image-small`}>
         <div
-          style={{ 
-            textShadow: '1px 1px 2px rgba(0,0,0,0.2)' }}
+          style={{
+            textShadow: '1px 1px 2px rgba(0,0,0,0.2)'
+          }}
           className='mt-2 font-semibold'>
           <SectionHeader text='Gott snack festival' />
         </div>
-        <div style={{ textShadow: '1px 1px 2px rgba(0,0,0,0.4)' }} className='lg:text-2xl md:text-2xl font-sans mb-5 text-sm text-white'>
+        <div style={{ textShadow: '1px 1px 2px rgba(0,0,0,0.8)' }} className='lg:text-2xl md:text-2xl font-sans mb-5 text-sm text-white'>
           Stockholm saknar en MYSIG stadsfestival.<br />  Följ med på resan mot en magisk kväll 5 augusti 2023.
           <br />
           Fri entré!
         </div>
-        <div className='grid grid-cols-2 gap-6'>
+        <div className='grid grid-cols-2 md:gap-6 gap-2'>
           <button className='mb-6'>
             <Link href="/festival#top" >
               <div className='patreon-button rounded-xl link flex justify-between px-4 py-3 items-center gap-1'>
--- pages/festival.tsx
+++ pages/festival.tsx
@@ -7,100 +7,63 @@ const Festival = () => {
     <div className='mx-3 md:mx-44'>
       <div id='top'>
       </div>
-      <div>
-        <div className='text-center'>
-          <LargeHeader headerText='Gott snack festival' />
-          <i><b>Första upplagan av Gott snack festivalen BLIR av lördagen den 5 augusti 2023!</b></i>
-        </div>
-        <div className='items-center grid md:grid-cols-2 grid-cols-1 md:gap-4'>
-          <div className='
+      <div className='text-center'>
+        <LargeHeader headerText='Gott snack festival' />
+        <i><b>Första upplagan av Gott snack festivalen BLIR av lördagen den 5 augusti 2023!</b></i>
+      </div>
+      <div className='items-center'>
+        <div className='
             text-center
             md:text-lg
             font-sans
             mb-5'>
-            <div>
-              <br />
-              <br />
-              Vi har fått tillstånd och har nu börjat jobbat med att boka artister och få in sponsorer!
-              Vi har ett tillstånd på 400 personer men skulle vilja kolla intresset! Om det är så att många fler är sugna så tänker vi att det är bra att öka den siffran i tillståndet!
-              <br />
-              <br />
-              I skrivande stund har vi tre bokade band!
-              <br />
+          <div>
+            <br />
+            Äntligen sjösätter vi drömprojektet Gott snack festival på riktigt! Vi hoppas att ni vill hjälpa oss göra detta möjligt så att det kan bli en underbar tradition!
+            För att gå runt kommer vi behöva sälja minst 600 stödbiljetter á 250 kronor! Vi kommer inte kräva biljetter för inträde men vi behöver ert stöd för att kunna genom genomföra denna folkfest!
+            <br />
+            <br />
+            Vi behöver även få in några företag som stöttar festivalen! Där finns möjlighet att köpa två olika typer av biljetter: stora eller lilla företagskramen!
+            Mejla oss på <b>festival@gottsnack.nu</b> så snackar vi upplägg!
+            <br />
+            <br />
+            Vi kommer inte neka någon inträde eller jaga plankare för vi vill att festivalen ska finnas för att ni vill att den ska finnas! Och då hoppas vi att ni vill stötta den med en stödbiljett!
+            Om vi inte får in den summa vi behöver för att gå runt (ca 280K) två veckor innan datumet för festivalen <b>(22/7)</b> så kommer evenemanget ställas in och alla får sina pengar tillbaka! Man tar alltså ingen risk genom att köpa en biljett!
+            <br />
+            <br />
+            <b>Vi måste alltså ha sålt ALLA stödbiljetter senast 22/7</b>
+            <br />
+            Detta är tack vare EasyArr som har ett fiffigt system som håller i pengarna tills deadline och sedan betalar tillbaka pengarna till varje enskild biljettköpare om evenemanget inte blir av pga att hela summan inte lyckades tjänas in.
+            <br />
+            <i>Pengarna tillbaka om festivalen ställs in!</i>
+            <br />
+            <br />
+            Följ gärna festivalen på <Link
+              href='https://www.instagram.com/gott_snack_festival/'
+              target='_blank'>
+              <b>Instagram</b>
+            </Link> och kolla in evenemanget på <Link
+              href='https://www.facebook.com/events/3412056322456299'
+              target='_blank'>
+              <b>Facebook</b>
+            </Link>
+            <br />
+            <br />
+            <div className='text-xl md:text-2xl'>
               <Link
-                href='https://www.instagram.com/division7band/'
+                href='https://app.easyarr.se/arrangemang/Gottsnackfestival.html'
                 target='_blank'>
-                <b><i>Division 7</i></b>
-                <br />
-              </Link><Link
-                href='https://www.instagram.com/rebecka_sandberg/'
-                target='_blank'>
-                <b><i>Rebecka Sandberg</i></b>
+                <b>Köp stödbiljetter här!</b>
               </Link>
-              <br />
-              <Link
-                href='https://www.instagram.com/hej.tiger/'
-                target='_blank'>
-                <b><i>Tiger</i></b>
-              </Link>
-              <br />
-              <br />
-              Förfrågade:
-              <br />
-              <Link
-                href='https://www.instagram.com/avantgardet_bandet/'
-                target='_blank'>
-                <b><i>Avantgardet</i></b>
-              </Link>
-              <br />
-              <Link
-                href='https://www.instagram.com/shoutoutloudsofficial/'
-                target='_blank'>
-                <b><i>Shout Out Louds</i></b>
-              </Link>
-              <br />
-              <br />
-              Utöver banden kommer det också finnas <b>Spådam</b>, <b>DJ</b>, <b>Foodtrucks</b> och <b>Konstutställning</b>.
-              <br />
-              <br />
-              TAW, Aasma och Boys & Ivy kunde tyvärr inte detta datum i år!
-              <br />
-              Har ni förslag på artister var inte blyga för att skicka på insta dm eller mejla på <b>festival@gottsnack.nu</b>
-              <br />
-              <br />
-              Känner ni konstnärer som vill visa upp sina alster får ni gärna tipsa oss!
-              Fler förslag på fler roliga saker på området som vore mysigt mottages tacksamt!
-              Om ni känner härliga människor som driver reko bolag som man kan sammarbeta med får ni gärna tipsa oss också!
-              <br />
-              <br />
-              Följ gärna festivalen på <Link
-                href='https://www.instagram.com/gott_snack_festival/'
-                target='_blank'>
-                <b>Instagram</b>
-              </Link> och kolla in evenemanget på <Link
-                href='https://www.facebook.com/events/3412056322456299'
-                target='_blank'>
-                <b>Facebook</b>
-              </Link>
-              <br />
-              <br />
-              <div className='text-xl md:text-2xl'>
-                <Link
-                  href='https://app.easyarr.se/arrangemang/Gottsnackfestival.html'
-                  target='_blank'>
-                  <b>Köp stödbiljetter här!</b>
-                </Link>
-              </div>
-              <br />
             </div>
           </div>
-          <div className='flex justify-center'>
-            <Image
-              src='/gs-festival.jpg'
-              alt=''
-              width={500}
-              height={300} />
-          </div>
+        </div>
+        <div className='flex justify-center'>
+          <Image
+            src='/gs-festival.jpg'
+            alt=''
+            width={400}
+            height={300} />
         </div>
       </div>
     </div>
